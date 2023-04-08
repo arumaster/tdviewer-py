7c94f42f5ddedc1061723edc7bcad50606794bbc
arumaster <127366706+arumaster@users.noreply.github.com>
2023-04-08 09:58:07 +0000

    複数選択に対応。フィルターを追加

--- tdviewer.py
+++ tdviewer.py
@@ -87,19 +87,55 @@ class TDViewer:
         sd.stop(True)
 
 # %%
-
 tdview = TDViewer(base_dir_path)
 
 # %% 
 # GUI
 #!%gui tk
-e = None
+
 import PySimpleGUI as sg
-lstbox = sg.Listbox(sound_ids,size=(50, 50))
-win = sg.Window("TDViewer", [[lstbox, [sg.Button("Play"), sg.Button("Stop")]]], resizable=True)
+import re
+import threading
+import itertools
+from queue import Queue
+import queue
+import threading
+import time
+
+stop_ev = threading.Event()
+def _play_thread(q:Queue):
+    while not stop_ev.is_set():
+        try:
+            audio = q.get(timeout=0.1)
+        except queue.Empty:
+            continue
+        try:
+            st = sd.get_stream()
+            if st.active :
+                # 終了イベントを確認
+                while True:
+                    if stop_ev.is_set():
+                        break
+                    if not st.active:
+                        break
+                    time.sleep(0.1)
+        except RuntimeError:
+            pass
+        for i, data in audio.samples.items():
+                    tdview.play(data)        
+    sd.stop(True)
+
+audio_q = Queue()    
+play_thread = threading.Thread(target=_play_thread, kwargs={"q":audio_q})
+play_thread.start()
+lstbox = sg.Listbox(sound_ids,size=(30, 20), key="-List-",expand_x=True, expand_y=True,select_mode=sg.LISTBOX_SELECT_MODE_EXTENDED)
+win = sg.Window("TDViewer", [[sg.Input(size=(30,1), enable_events=True, key="-INPUT-")],
+                               [lstbox], [sg.Button("Play"), sg.Button("Stop")] ,[sg.Button("Reset")]], resizable=True)
 while True:
     event, values = win.read()
+    print(f"event:{event}")
     if event == sg.WINDOW_CLOSED:
+        stop_ev.set()
         if settings_path.exists() == False:
             ok_or_cancel = sg.popup_ok_cancel("指定したパスを設定ファイルに保存しますか？")
             if ok_or_cancel == "OK":
@@ -107,19 +143,42 @@ while True:
                     json.dump({"GAME_DATA_PATH": str(base_dir_path)}, f)
         break
     if event == "Play":
-        v:str = values[0][0]
-        try:
-            audio = tdview.load_audio(v.lower() + ".abap")
-            for i,data in audio.samples.items():
-                tdview.play(data)
-        except FileNotFoundError as err:
-            sg.popup(f"ファイル({err.args[0]})が見つかりませんでした", title="ファイルが見つかりませんでした。")
-        except Exception as err:
-            e = err
-            print(err)
-            break
+        if play_thread.is_alive() == False:
+            stop_ev.clear()
+            audio_q = Queue()
+            play_thread = threading.Thread(target=_play_thread, kwargs={"q":audio_q})
+            play_thread.start()
+        selections = values["-List-"]
+        print(f"selections:{selections}")
+        filenames = [ s.lower() + ".abap" for s in selections]
+        missing_filenames = []
+        for filename in filenames:
+            if (base_dir_path / filename).exists() == False:
+                print(f"File Not Found: {filename}")
+                missing_filenames.append(filename)
+        play_filenames = [n for n in filenames if n not in missing_filenames]
+        for sel in play_filenames:
+            try:
+                print(sel)
+                audio = tdview.load_audio(sel)
+                audio_q.put(audio)
+            except FileNotFoundError as err:
+                sg.popup(f"ファイル({err.args[0]})が見つかりませんでした", title="ファイルが見つかりませんでした。")
+            except Exception as err:
+                print(err)
+                break
     elif event == "Stop":
-        tdview.stop()
+        sd.stop()
+        stop_ev.set()
+        
+    elif event == "-INPUT-":
+        print("-INPUT-")
+        if values["-INPUT-"] != '':
+            s:str = values["-INPUT-"]
+            r = re.compile(s,re.IGNORECASE)
+            ids = [ i for i in sound_ids if r.search(i)]
+            win["-List-"].update(ids)
+    elif event == "Reset":
+            win["-INPUT-"].update(value="")
+            win["-List-"].update(sound_ids)
 win.close()
-
-
